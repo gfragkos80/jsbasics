45aa308fa5129cce5f4b668846cd4c38e27e87da
gfragkos80 <g.frangos80@gmail.com>
2025-03-10 14:33:30 -0400

    add menu lists

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,21 @@
+// Menu tables
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+// Iterate breakfast menu
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+// traverse main course menu
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+// Iterate dessert Menu
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
