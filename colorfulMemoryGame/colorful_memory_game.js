673bd88ff7566203fb4ed528789ee36d82ca23e3
Georgios Fragkos <g.frangos80@gmail.com>
2025-03-18 13:49:36 +0200

    add comments

--- colorfulMemoryGame/colorful_memory_game.js
+++ colorfulMemoryGame/colorful_memory_game.js
@@ -12,6 +12,7 @@ const gameContainer = document.getElementById('game-container');
 const scoreElement = document.getElementById('score');
 const timerElement = document.getElementById('timer');
 
+// 
 function generateCards() {
     for (const color of cards) {
         const card = document.createElement('div');
@@ -33,8 +34,9 @@ function shuffle(array) {
 }
 
 function handleCardClick(event) {
-    //get element from event
+    //get element from event. The event trigger defines the selected card
     const card = event.target;
+
     //Check if card & already matched
     if (!card.classList.contains('card') || card.classList.contains('matched')) {
         return;
@@ -42,6 +44,7 @@ function handleCardClick(event) {
     // set text from color
     card.textContent = card.dataset.color;
     card.style.backgroundColor = card.dataset.color;
+
     selectedCards.push(card);
     if (selectedCards.length === 2) {
         setTimeout(checkMatch, 500);
@@ -49,13 +52,16 @@ function handleCardClick(event) {
 }
 
 function checkMatch() {
+    // Decontruct selected cards
     const [card1, card2] = selectedCards;
+
     if (card1.dataset.color === card2.dataset.color) {
         card1.classList.add('matched');
         card2.classList.add('matched');
         score += 2;
         scoreElement.textContent = `Score: ${score}`;
-    } else {
+
+    } else { // Unmatch them
         card1.textContent = '?';
         card2.textContent = '?';
         card1.style.backgroundColor = '#ddd';
@@ -74,15 +80,21 @@ function startGame() {
     
     startGameTimer(timeLeft);
     
+    // Suffle cards
     cards = shuffle(colors.concat(colors));
+
     selectedCards = [];
     gameContainer.innerHTML = '';
+    // Generate Cards divs
     generateCards();
+    // Add listener to game container
     gameContainer.addEventListener('click', handleCardClick);
 }
 
 function startGameTimer(timeLeft) {
+
     timerElement.textContent = `Time Left: ${timeLeft}`;
+    
     gameInterval = setInterval(() => {
         timeLeft--;
         timerElement.textContent = `Time Left: ${timeLeft}`;
